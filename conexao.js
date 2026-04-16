d9a7afc0b09a75289b79621d03cce5726e7708ea
eduardoberettarod <eduardo.berettarod@gmail.com>
2026-04-16 14:16:06 -0300

    Fix: configuração para Vercel com reconexão automática

--- conexao.js
+++ conexao.js
@@ -1,4 +1,5 @@
 let mysql = require('mysql')
+
 let conexao = mysql.createConnection({
     host: process.env.HOST,
     port: process.env.PORTA || 4000,                    
@@ -8,13 +9,30 @@ let conexao = mysql.createConnection({
     ssl: { rejectUnauthorized: false } 
 })
 
-conexao.connect(function (erro) {
-    if (erro) {
-        console.log("Deu ruim na conexão \n");
-        throw erro;
-    } else {
-        console.log("Conexão deu bom \n")
+function conectar() {
+    conexao.connect(function (erro) {
+        if (erro) {
+            console.error("Erro na conexão com banco:", erro.message);
+            setTimeout(conectar, 5000); // Reconectar em 5s
+        } else {
+            console.log("Conexão com banco estabelecida com sucesso");
+        }
+    })
+}
+
+conexao.on('error', function(err) {
+    console.error("Erro de conexão detectado:", err.message);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+        conectar();
     }
-})
+    if(err.code === 'PROTOCOL_ENQUEUE_AFTER_FATAL_ERROR') {
+        conectar();
+    }
+    if(err.code === 'PROTOCOL_ENQUEUE_AFTER_FATAL_ERROR') {
+        conectar();
+    }
+});
+
+conectar();
 
 module.exports = conexao
